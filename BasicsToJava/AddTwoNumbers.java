2f51f79f6d8f521d471709ab6e38bb6359ca366e
Modita <moditav.rana@gmail.com>
2022-11-06 03:09:51 +0530

    Create AddTwoNumbers.java

---
+++ BasicsToJava/AddTwoNumbers.java
@@ -0,0 +1,14 @@
+package BasicsToJava;
+
+public class AddTwoNumbers {
+    public static void main(String[] args){
+        // declare variables
+        int a = 5;
+        int b = 9;
+        int c = a + b;
+        // Print c
+
+        System.out.println(c);
+    }
+}
+
